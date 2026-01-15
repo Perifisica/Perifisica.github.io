a8e1a602656c26d5874072d9da942f3326f5b6f3
Emanuele Urso <ursomanuele@gmail.com>
2026-01-15 09:44:11 +0100

    ho fatto un casino con le commit, ora ho risolto

--- colonna_sinistra_indice/indice.js
+++ colonna_sinistra_indice/indice.js
@@ -12,7 +12,7 @@ function OpenLatex() {
   document.getElementById("latex").classList.toggle("show");
 }
 
-
+/*
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbutton')) {
@@ -26,3 +26,4 @@ window.onclick = function(event) {
     }
   }
 }
+  */

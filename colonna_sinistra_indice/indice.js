614c4b91a8cdcb34471a3903e973b0971ef4a9cf
Emanuele Urso <ursomanuele@gmail.com>
2026-01-16 01:55:11 +0100

    chevron funzionanti bene

--- colonna_sinistra_indice/indice.js
+++ colonna_sinistra_indice/indice.js
@@ -1,6 +1,6 @@
 function Chevron(contentId, chevronId) {
-  const content = document.querySelector("#" + contentId);
-  const chevron = document.querySelector("#" + chevronId);
+  const content = document.getElementById(contentId);
+  const chevron = document.getElementById(chevronId);
 
   console.log(content);
 
@@ -8,31 +8,27 @@ function Chevron(contentId, chevronId) {
   console.log(isOpen);
   if (isOpen) {
     content.style.maxHeight = null;
-    chevron.style.transform = "rotate(0deg)";
-    chevron.style.setProperty("left", "0rem");
-    chevron.style.setProperty("top", "-.5rem");
+    chevron.style.transform = "rotate(0deg) translate(0rem, 0px)";
   } else {
     content.style.maxHeight = content.scrollHeight + "px";
-    chevron.style.transform = "rotate(90deg)";
-    chevron.style.setProperty("left", ".5rem");
-    chevron.style.setProperty("top", "0.5px");
+    chevron.style.transform = "rotate(90deg) translate(+.4rem, 0px)";
   }
 }
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function OpenTeoria() {
-  document.getElementById("teoria-button").classList.toggle("show");
+  document.getElementById("teoria-content").classList.toggle("show");
   Chevron("teoria-content", "teoria-chevron");
 }
 
 function OpenEsperimenti() {
-  document.getElementById("esperimenti-button").classList.toggle("show");
+  document.getElementById("esperimenti-content").classList.toggle("show");
   Chevron("esperimenti-content", "esperimenti-chevron");
 }
 
 function OpenLatex() {
-  document.getElementById("latex-button").classList.toggle("show");
+  document.getElementById("latex-content").classList.toggle("show");
   Chevron("latex-content", "latex-chevron");
 }
 

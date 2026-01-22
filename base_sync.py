e96eaefc284eb75b7b617781a2a9fc51de067263
Emanuele Urso <ursomanuele@gmail.com>
2026-01-22 19:01:00 +0100

    sincronizzazione riuscita

--- base_sync.py
+++ base_sync.py
@@ -1,43 +1,73 @@
-# import re
-
-# BASE_FILE = "base.html"
-# TARGET_FILE = "pagina.html"
-
-# def estrai_main(html):
-#     match = re.search(r"<main[\s\S]*?>[\s\S]*?</main>", html, re.IGNORECASE)
-#     if not match:
-#         raise ValueError("Tag <main> non trovato")
-#     return match.group(0)
-
-# def sostituisci_main(html, nuovo_main):
-#     return re.sub(
-#         r"<main[\s\S]*?>[\s\S]*?</main>",
-#         nuovo_main,
-#         html,
-#         flags=re.IGNORECASE
-#     )
-
-# if __name__ == "main":
-#     with open(BASE_FILE, "r", encoding="utf-8") as f:
-#         base_html = f.read()
-
-#     with open(TARGET_FILE, "r", encoding="utf-8") as f:
-#         target_html = f.read()
-
-#     main_originale = estrai_main(target_html)
-#     html_finale = sostituisci_main(base_html, main_originale)
-
-#     with open(TARGET_FILE, "w", encoding="utf-8") as f:
-#         f.write(html_finale)
-
-#     print("Sincronizzazione completata.")
-
 import os
+import re
 
-htmls_path = r"./pages"
 
-for file in os.scandir(htmls_path):
-    if file.is_file():
-        with open(file.path, "r") as file_io:
-            print("Content of", file.name, ":")
-            print(file_io.read())
+def read_html_files(htmls_path):
+    pages = []
+    pages_name = []
+    for file in os.scandir(htmls_path):
+        if file.is_file():
+            with open(file.path, "r") as file_input:
+                if file.name == "index.html":
+                    index_html = file_input.read()
+                else:
+                    page_name = file.name
+                    page = file_input.read()
+                    pages_name.append(page_name)
+                    pages.append(page)
+        else:
+            print(file.name, " non è un file")
+    return index_html, pages, pages_name
+
+
+def extract_html_base(index_html):
+    pattern_pre_title = r'^(.*?<title>)'
+    pre_title = re.search(pattern_pre_title, index_html, re.DOTALL).group(1)
+    #print(pre_title, "\n")
+
+    pattern_post_title_to_article = r'(</title>.*?<article>)'
+    post_title_to_article = re.search(pattern_post_title_to_article, index_html, re.DOTALL).group(1)
+    #print(post_title_to_middle_column)
+
+    pattern_post_article = r'(</article>.*)'
+    post_article = re.search(pattern_post_article, index_html, re.DOTALL).group(1)
+
+    return [pre_title, post_title_to_article, post_article]
+
+
+def extract_middle_column(page):
+    pattern_title = r'<title>(.*?)</title>'
+    title = re.search(pattern_title, page, re.DOTALL).group(1)
+    #print(title)
+
+    pattern_article = r'<article>(.*?)</article>'
+    article = re.search(pattern_article, page, re.DOTALL).group(1)
+    #print(article)
+
+    return [title, article]
+
+def rewrite_all(file_path, page_name, index_parts, page_parts):
+    file = file_path + page_name
+    all_text = index_parts[0] + page_parts[0] + index_parts[1] + page_parts[1] + index_parts[2]
+    #print(all_text)
+
+    with open(file, "w") as file_ouput:
+        file_ouput.write(all_text)
+
+    print("Sicronizzazione eseguita su ", page_name)
+    
+
+
+                    
+
+
+if __name__ == "__main__":
+    HTMLS_PATH = r"./pages/"
+    INDEX_HTML, PAGES, PAGES_NAME = read_html_files(HTMLS_PATH)
+    CLEAN_INDEX = extract_html_base(INDEX_HTML)
+    CLEAN_PAGES = []
+    for iterator , page in enumerate(PAGES):
+        #print(PAGES_NAME[iterator], "\n")
+        CLEAN_PAGE = extract_middle_column(page)
+        rewrite_all(HTMLS_PATH, PAGES_NAME[iterator], CLEAN_INDEX, CLEAN_PAGE)
+

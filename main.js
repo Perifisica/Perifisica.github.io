401a31bdfa492350195278662ae1faf4a68440ac
Emanuele Urso <ursomanuele@gmail.com>
2026-02-08 01:59:44 +0100

    velocizzare il sito
    ho rimosso i font in più, cambiato formato a quelli usati per rendere più veloce il sito

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import { includeHTML } from "./include_html.js";
+await includeHTML();
+
+
 import { Search } from "/barra_ricerca/search.js";
 import { init as Pulsanti } from "/colonna_destra/pulsanti.js"
 import { init as Indice } from "/colonna_sinistra_indice/indice.js"
 import { init as Mobile } from "/mobile/mobile.js";
 import { init as Allarga } from "./image_enlarge.js";
-
-await includeHTML();
 
 Search();
 Pulsanti(document);
